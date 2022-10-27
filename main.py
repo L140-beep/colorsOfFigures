0ee521c32a96a76c49e1e964e83f6db8b53f949c
L140-beep <kartoshka0302@mail.ru>
2022-10-27 16:07:52 +0800

    added mergeColors function

--- main.py
+++ main.py
@@ -4,6 +4,21 @@ import matplotlib.pyplot as plt
 from skimage.measure import label, regionprops
 from skimage import color
 
+def mergeColors(colors : DefaultDict) -> dict:
+    result = colors.copy()
+    
+    sorted_keys = sorted(result.keys())
+    diff = np.diff(sorted_keys)
+    delta = np.std(diff) * 2
+
+    for key in range(1, len(sorted_keys)):
+        dc = sorted_keys[key] - sorted_keys[key - 1]
+        
+        if(dc < delta):
+            result[sorted_keys[key]] += result[sorted_keys[key-1]]
+            result.pop(sorted_keys[key - 1], None)
+    return dict(result)
+
 image = plt.imread('files/balls_and_rects.png')
 
 hsv = color.rgb2hsv(image)[:, :, 0]
@@ -16,40 +31,26 @@ props = regionprops(labeled)
 
 circles = DefaultDict(lambda: 0)
 rects = DefaultDict(lambda: 0)
+
 count_rects = 0
 count_circles = 0
 
 for figure in range(0, labeled.max()):
-       
     cy, cx = props[figure].centroid
     color = hsv[int(cy), int(cx)]
     
     if (props[figure].eccentricity):
-        count_rects += 1
         rects[color] += 1
+        count_rects += 1
     else:
         count_circles += 1
         circles[color]+=1
 
-print(count_rects, count_circles, labeled.max())
 
+rects = mergeColors(rects)
+circles = mergeColors(circles)
 
-sorted_rect = sorted(rects.keys())
-diff_rect = np.diff(sorted_rect)
-delta_rect = np.std(diff_rect) * 2
-
-for key in range(1, len(sorted_rect)):
-    dc = sorted_rect[key] - sorted_rect[key - 1]
-    
-    if(dc < delta_rect):
-        rects[sorted_rect[key]] += rects[sorted_rect[key-1]]
-        rects.pop(sorted_rect[key - 1], None)
-
-print(dict(rects))
-# plt.subplot(121)
-# plt.title("rects")
-# plt.plot(diff_rect)
-# plt.subplot(122)
-# plt.title("circles")
-# plt.plot(sorted(rects), "o")
-# plt.show()
+print("Прямоугольники:", count_rects)
+print(rects)
+print("Круги:", count_circles)
+print(circles)
